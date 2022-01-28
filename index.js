90cd36726aa254aabc0df433d1d68976c8883db2
Brandon Dega <brandondega@gmail.com>
2022-01-28 12:01:52 -0600

    fixed brackets

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ let firstAnswer  = window.prompt('Do you head left or right?')
          Do you follow it, or continue on your path?`)
         if(secondAnswer === 'follow') {
             let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
-        } 
             if(thirdAnswer === 'stay') {
                 window.alert(`You live happily amongst the cats for the rest of your days.`)
             }
             else if(thirdAnswer ==='spread the word') {
                 window.alert(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
             }
+        }
         else if(secondAnswer === 'continue') {
             let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
         }
@@ -24,10 +24,10 @@ let firstAnswer  = window.prompt('Do you head left or right?')
             }
     } 
     else if(firstAnswer === 'right') {
-         let secondAnswer = window.prompt(`You come across a snoring dragon. 
-         On the other side of him, you see a shiny chest of treasure. Another path would 
+        let secondAnswer = window.prompt(`You come across a snoring dragon. 
+        On the other side of him, you see a shiny chest of treasure. Another path would 
          lead you away from the dragon altogether. Which path do you take?`)
-    }
+    
         if(secondAnswer === 'past the dragon') {
             let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. You only have a moment to respond, to stay or run.`)
         }
@@ -46,4 +46,5 @@ let firstAnswer  = window.prompt('Do you head left or right?')
             else if(thirdAnswer === 'pick it') {
                 window.alert(`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`)
             }
+        }
     
