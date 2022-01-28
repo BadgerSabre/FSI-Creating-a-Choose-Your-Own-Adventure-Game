94e5617430f6c71536105f9b55cd6bc56008d52e
Brandon Dega <brandondega@gmail.com>
2022-01-27 19:30:31 -0600

    still not working?

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ let firstAnswer  = window.prompt('Do you head left or right?')
                 window.alert(`You live happily amongst the cats for the rest of your days.`)
             }
             else if(thirdAnswer ==='spread the word') {
-                let fourthAnswer = window.alert(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
+                window.alert(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
             }
         else if(secondAnswer === 'continue') {
             let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
         }
             if(thirdAnswer === 'ladder') {
-                let fourthAnswer = window.alert(`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)
+                window.alert(`After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.`)
             }
             else if(thirdAnswer === 'staircase') {
-                let fourthAnswer = window.alert(`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
+                window.alert(`After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.`)
             }
     } 
     else if(firstAnswer === 'right') {
@@ -32,18 +32,18 @@ let firstAnswer  = window.prompt('Do you head left or right?')
             let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. You only have a moment to respond, to stay or run.`)
         }
             if(thirdAnswer === 'stay') {
-                let fourthAnswer = window.alert(`You and the dragon have an uplifting conversation about local politics and become lifelong friends.`)
+                window.alert(`You and the dragon have an uplifting conversation about local politics and become lifelong friends.`)
             }
             else if(thirdAnswer === 'run') {
-                let fourthAnswer = window.alert(`Quickly, you run back to the cave's entrance. Sheepish, you return home.`)
+                window.alert(`Quickly, you run back to the cave's entrance. Sheepish, you return home.`)
             }
         else if(secondAnswer === 'away from dragon') {
             let thirdAnswer = window.prompt(`After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`)
         }
             if(thirdAnswer === 'draw it') {
-                let fourthAnswer = window.alert(`You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.`)
+                window.alert(`You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.`)
             }
             else if(thirdAnswer === 'pick it') {
-                let fourthAnswer = window.alert(`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`)
+                window.alert(`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`)
             }
     
